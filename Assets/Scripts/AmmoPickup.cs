23bcf33d1af41f5ebeab7f36ea4f799a5e8aed9a
Gil Morhaim <gil.morhaim@herolo.co.il>
2023-10-04 14:01:53 +0300

    Add Ammo asset package and complete ammo pickup prefabs including floating animation

--- Assets/Scripts/AmmoPickup.cs
+++ Assets/Scripts/AmmoPickup.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 public class AmmoPickup : Pickup
 {
+    [Header("Ammo Configurations")]
     [SerializeField] private int _ammoAmount = 5;
     [SerializeField] private AmmoType _ammoType;
     [SerializeField] private Ammo _ammo;
